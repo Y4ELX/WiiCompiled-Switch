1d803a91ef1a0095b6a41f69e10bbfa89f9822b5
yashin-sh <yacinesediri@gmail.com>
2026-09-13 20:27:38 +0200

    fix: register NANDReadAsync HLE

--- include/switch_os_hle_traits.hpp
+++ include/switch_os_hle_traits.hpp
@@ -68,6 +68,7 @@ struct KnownNativeCpuCall<0x801B0220u> {
 // catalogue.
 #include "switch_nand_hle_traits.hpp"
 #include "switch_nand_open_async_hle_traits.hpp"
+#include "switch_nand_read_async_hle_traits.hpp"
 #include "switch_dvd_hle_traits.hpp"
 #include "switch_dvd_low_hle_traits.hpp"
 #include "switch_esp_hle_traits.hpp"
